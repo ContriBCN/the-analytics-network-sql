f5995eafba4b132b0e9de2c3b78cc7869173fc06
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-08 22:57:21 +0200

    Update market_count.sql

--- parte-3/project/retail-data-warehouse/fct/market_count.sql
+++ parte-3/project/retail-data-warehouse/fct/market_count.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS stg.market_count;
 CREATE TABLE stg.market_count
                  (
                               tienda SMALLINT
-                            , fecha  INTEGER
+                            , fecha  INTEGER PRIMARY KEY
                             , conteo SMALLINT
                  );
