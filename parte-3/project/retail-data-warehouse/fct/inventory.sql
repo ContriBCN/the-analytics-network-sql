c9bd37dc1856f0affe221ba490c787fbdeb6e819
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-08 22:43:36 +0200

    Create inventory.sql

---
+++ parte-3/project/retail-data-warehouse/fct/inventory.sql
@@ -0,0 +1,8 @@
+CREATE TABLE new.inventory
+                 (
+                              tienda  SMALLINT 
+                            , sku     VARCHAR(10) PRIMARY KEY
+                            , fecha   DATE
+                            , inicial SMALLINT
+                            , final   SMALLINT
+                 );
