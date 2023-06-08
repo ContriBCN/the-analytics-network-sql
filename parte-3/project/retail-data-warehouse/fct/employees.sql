ceb7a5ebbca785762eb1df5e812da2cd42f1f29f
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-08 22:38:10 +0200

    Create employees.sql

---
+++ parte-3/project/retail-data-warehouse/fct/employees.sql
@@ -0,0 +1,12 @@
+CREATE  TABLE stg.employees (
+						id serial primary key,
+						nombre VARCHAR,
+						apellido VARCHAR,
+						fecha_entrada DATE,
+						fecha_salida DATE,
+						telefono BIGINT,
+						pais VARCHAR,
+						provincia VARCHAR,
+						codigo_tienda INTEGER,
+						posicion VARCHAR
+ );
