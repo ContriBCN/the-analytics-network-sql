f30a37719ecb5dba81c1f89a5c2568f143075a42
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-11 22:51:16 +0200

    Delete monthly_average_fx_rate.sql

--- parte-3/project/retail-data-warehouse/fct/monthly_average_fx_rate.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE fct.monthly_average_fx_rate
-                 (
-                              mes                 DATE PRIMARY KEY
-                            , cotizacion_usd_peso DECIMAL
-                            , cotizacion_usd_eur DECIMAL
-                            , cotizacion_usd_uru  DECIMAL
-                 );
