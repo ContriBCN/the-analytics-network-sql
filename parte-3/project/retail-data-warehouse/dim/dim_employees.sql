d1cc86b25c4ea3dcddef32b4f35ceac0a44558c8
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-11 22:48:43 +0200

    Create dim_employees.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_employees.sql
@@ -0,0 +1,14 @@
+CREATE  TABLE dim.employees (
+						id serial primary key,
+						nombre VARCHAR,
+						apellido VARCHAR,
+						fecha_entrada DATE,
+						fecha_salida DATE,
+						telefono BIGINT,
+						pais VARCHAR,
+						provincia VARCHAR,
+						codigo_tienda INTEGER,
+						CONSTRAINT codigo_tienda FOREIGN KEY (codigo_tienda)
+						REFERENCES new.store_master,
+						posicion VARCHAR
+ );
