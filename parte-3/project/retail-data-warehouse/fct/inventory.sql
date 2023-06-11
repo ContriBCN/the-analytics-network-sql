0c4bc1c8a69c73b2c97c4271fb7cbf647222c4d0
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-11 22:50:02 +0200

    Update inventory.sql

--- parte-3/project/retail-data-warehouse/fct/inventory.sql
+++ parte-3/project/retail-data-warehouse/fct/inventory.sql
@@ -1,4 +1,4 @@
-CREATE TABLE new.inventory
+CREATE TABLE fct.inventory
                  (
                               tienda  SMALLINT 
                             , sku     VARCHAR(10) PRIMARY KEY
