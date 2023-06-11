3052bd46b4121c4d7cab665b9f8959e0208caffa
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-11 23:06:08 +0200

    Update cost.sql

--- parte-3/project/retail-data-warehouse/stg/cost.sql
+++ parte-3/project/retail-data-warehouse/stg/cost.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS stg.cost;
-    
-CREATE TABLE stg.cost
+CREATE TABLE public.cost
                  (
                               codigo_producto    VARCHAR(10)
                             , costo_promedio_usd DECIMAL
