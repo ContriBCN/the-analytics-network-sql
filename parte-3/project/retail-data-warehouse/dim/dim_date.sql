6c84c1368cc38103205d533d4daa43adfce51874
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-11 22:47:30 +0200

    Create dim_date.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_date.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS fct.date
+(
+    fecha date,
+    mes double precision,
+    year double precision,
+    dia_semana text COLLATE pg_catalog."default",
+    is_weekend boolean,
+    nombre_mes text COLLATE pg_catalog."default",
+    fiscal_year double precision,
+    fiscal_year2 text COLLATE pg_catalog."default",
+    fiscal_quarter text COLLATE pg_catalog."default",
+    "año_anterior" date,
+    PRIMARY KEY (fecha)
+)
