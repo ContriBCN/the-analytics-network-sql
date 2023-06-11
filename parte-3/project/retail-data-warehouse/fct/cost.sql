5de702cb9c4e9169a846e20e698774f642a6c72a
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-11 22:46:56 +0200

    Delete cost.sql

--- parte-3/project/retail-data-warehouse/fct/cost.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS fct.cost;
-    
-CREATE TABLE new.cost
-                 (
-                              codigo_producto    VARCHAR(10)
-                            , costo_promedio_usd DECIMAL
-                            , PRIMARY KEY (codigo_producto)
-                 );
