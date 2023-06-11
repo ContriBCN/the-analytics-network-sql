b0ef829a39d2ad4dd595a5a5769e2c55ef4d928f
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-11 22:46:41 +0200

    Create dim_cost.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS fct.cost;
+    
+CREATE TABLE new.cost
+                 (
+                              codigo_producto    VARCHAR(10)
+                            , costo_promedio_usd DECIMAL
+                            , PRIMARY KEY (codigo_producto)
+                 );
