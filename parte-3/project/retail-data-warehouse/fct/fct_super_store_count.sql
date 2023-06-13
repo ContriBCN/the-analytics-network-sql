372fba52f5b0fa96e50a8348dd836662d033c9f6
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-13 22:58:36 +0200

    Update fct_super_store_count.sql

--- parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
@@ -3,4 +3,5 @@ CREATE TABLE fct.super_store_count
                               tienda SMALLINT
                             , fecha  VARCHAR(10)
                             , conteo SMALLINT
+				                    , primary key (fecha, tienda)
                  );
