b622c5b13e61e29a8650912d5e07d6132b37c32a
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-06-18 13:09:09 +0200

    Delete super_store_count.sql

--- parte-3/project/retail-data-warehouse/stg/super_store_count.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS stg.super_store_count;
-    
-CREATE TABLE stg.super_store_count
-                 (
-                              tienda SMALLINT
-                            , fecha  VARCHAR(10)
-                            , conteo SMALLINT
-                 );
