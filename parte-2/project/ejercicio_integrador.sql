1d202042d3262762b09c5906d14241bd9ba2830f
Héctor <122044922+ContriBCN@users.noreply.github.com>
2023-07-01 22:16:02 +0200

    Update ejercicio_integrador.sql

--- parte-2/project/ejercicio_integrador.sql
+++ parte-2/project/ejercicio_integrador.sql
@@ -34,195 +34,181 @@
 -- El periodo fiscal de la empresa empieza el primero de febrero.
 -- Las metricas tienen que estar calculadas en dolares.
 
-create table stg.source_truth2 as 
-with cte_philips as (
-	select sum(cantidad) as cantidad_total, 
-	nombre
-	from stg.product_master pm 
-	left join stg.order_line_sale ols 
+create table stg.tpintegrador2 as
+with cte_philips as 
+(
+	select 
+		orden,
+		producto,
+		nombre,
+		200 / count(1) over() as adjustment
+	from stg.order_line_sale ols
+	left join stg.product_master pm 
 		on pm.codigo_producto = ols.producto
-	where nombre like '%PHILIPS%' 
-	group by nombre
+	where lower(pm.nombre) like '%philips%'
 ),
-ingreso_philips as (
-	select 200 / cantidad_total as ingreso_extra,
-	nombre
-	from cte_philips
-),
-cte_conteo as (
-	select sum (conteo) as conteo, tienda, fecha
-	from stg.super_store_count
-	group by tienda, fecha
-	order by fecha
+cte_returns as (
+	select 
+		orden_venta as orden, 
+		item, 
+		min(cantidad) as qty_returned
+	from stg.return_movements
+	group by 
+		orden_venta, 
+		item
 ),
 cte_ventas as (
 	select 
-		fecha,
-		tienda,
-		producto,
-		sum (cantidad) as qty
-	from stg.order_line_sale ols
-	group by 
-		fecha,
-		tienda,
-		producto
-),
-cte_DOH as (
-	select 
-		v1.tienda,
-		v1.producto,
-		v1.fecha,
-		sum (case when v1.fecha - v2.fecha <= 6 then v2.qty else 0 end) as ventas_promedio_7dias
-	from cte_ventas v1
-	left join cte_ventas v2
-		on v1.tienda = v2.tienda
-		and v1.producto = v2.producto
-		and v2.fecha <= v1.fecha
-	group by 
-		v1.tienda,
-		v1.producto,
-		v1.fecha
-)
-	select 
+		ols.fecha,
 		ols.producto,
-		ingreso_extra,
-		ols.fecha ,
-		ols.tienda,
-		ols.orden,
 		ols.cantidad,
-		venta / (case
-			when moneda = 'EUR' then fx.cotizacion_usd_eur
-			when moneda = 'ARS' then fx.cotizacion_usd_peso
-			when moneda = 'URU' then fx.cotizacion_usd_uru
-			end) ventas_usd,
-		descuento / (case
-			when moneda = 'EUR' then fx.cotizacion_usd_eur
-			when moneda = 'ARS' then fx.cotizacion_usd_peso
-			when moneda = 'URU' then fx.cotizacion_usd_uru
-			end) descuento_usd,
-		impuestos / (case
-			when moneda = 'EUR' then fx.cotizacion_usd_eur
-			when moneda = 'ARS' then fx.cotizacion_usd_peso
-			when moneda = 'URU' then fx.cotizacion_usd_uru
-			end) impuestos_usd,
-		creditos / (case
-			when moneda = 'EUR' then fx.cotizacion_usd_eur
-			when moneda = 'ARS' then fx.cotizacion_usd_peso
-			when moneda = 'URU' then fx.cotizacion_usd_uru
-			end) creditos_usd,
+		pm.nombre,
+		ols.orden,
+		ols.tienda,
 		sm.pais,
-		sm.provincia,
-		sm.nombre,
+		sm.nombre as nombre_tienda,
 		pm.categoria,
 		pm.subcategoria,
 		pm.subsubcategoria,
-		pm.nombre as nombre_producto,
-		s.nombre as proveedor,
-		(inicial+final)/2 AS inventario_promedio,
-		extract (day from d.fecha) as dia,
-		d.fecha as fecha2,
-		d.mes,
-		d.year,
-		d.fiscal_year,
-		d.fiscal_quarter,
-		(ols.cantidad * costo_promedio_usd) as costo_promedio,
-		rm.cantidad as devoluciones,
-		cte_conteo.conteo,
-		cte_DOH.ventas_promedio_7dias
+		stg.convert_usd(ols.moneda, ols.venta, ols.fecha) as venta_usd,
+		stg.convert_usd(ols.moneda, ols.descuento, ols.fecha) as descuentos_usd, 
+		stg.convert_usd(ols.moneda, ols.creditos, ols.fecha) as creditos_usd,
+		stg.convert_usd(ols.moneda, ols.impuestos, ols.fecha) as impuestos_usd, 
+		extract (day from d1.fecha) as dia,
+		d1.fecha as fecha2,
+		d1.mes,
+		d1.year, 
+		d1.fiscal_year,
+		d1.fiscal_quarter,
+		cte_philips.adjustment as adjustment,
+		cte_returns.qty_returned,
+		ols.cantidad * costo_promedio_usd as costo_usd
 	from stg.order_line_sale ols
-	left join stg.inventory inv
-		on ols.producto = inv.sku
-		and ols.tienda = inv.tienda 
-		and ols.fecha = inv.fecha 
-	left join stg.monthly_average_fx_rate fx
-		on fx.mes = date(date_trunc ('month',ols.fecha))
-	left join stg.store_master sm
-		on inv.tienda = sm.codigo_tienda
 	left join stg.product_master pm
-		on inv.sku = pm.codigo_producto
-	left join stg.suppliers s
-		on inv.sku = s.codigo_producto
-		and s.is_primary = 'True'
-	left join stg.date d
-		on inv.fecha = d.fecha
-	left join stg.cost c
-		on inv.sku = c.codigo_producto
-	left join stg.view_return_movements rm
-		on inv.sku = rm.item
-		and ols.orden = rm.orden_venta
-	left join ingreso_philips
-		on pm.nombre = ingreso_philips.nombre
-	left join cte_conteo
-		on ols.tienda = cte_conteo.tienda
-		and ols.fecha = cte_conteo.fecha::date
-	left join cte_DOH
-		on ols.tienda = cte_DOH.tienda
-		and ols.producto = cte_DOH.producto
-		and ols.fecha = cte_DOH.fecha
+		on pm.codigo_producto = ols.producto
+	left join stg.store_master sm
+		on sm.codigo_tienda = ols.tienda
+	left join stg.cost c1
+		on c1.codigo_producto = ols.producto
+	left join stg.date d1
+		on d1.fecha = ols.fecha
+	left join cte_philips 
+		on cte_philips.producto = ols.producto
+		and cte_philips.orden = ols.orden
+	left join cte_returns
+		on cte_returns.orden = ols.orden
+		and cte_returns.item = ols.producto 
+	left join stg.suppliers sp
+		on sp.codigo_producto = ols.producto 
+	where sp.is_primary is true 
+)
+select 
+	cte_ventas.fecha,
+	cte_ventas.producto,
+	cte_ventas.cantidad,
+	cte_ventas.nombre,
+	cte_ventas.tienda,
+	cte_ventas.pais,
+	cte_ventas.nombre as nombre_tienda,
+	cte_ventas.categoria,
+	cte_ventas.subcategoria,
+	cte_ventas.subsubcategoria,
+	extract (day from cte_ventas.fecha) as dia,
+	cte_ventas.fecha as fecha2,
+	cte_ventas.mes,
+	cte_ventas.year, 
+	cte_ventas.fiscal_year,
+	cte_ventas.fiscal_quarter,
+	cte_ventas.adjustment as adjustment,
+	cte_ventas.qty_returned,
+	count(distinct cte_ventas.orden) as countd_orden,
+	sum(cantidad) qty,
+	sum (venta_usd) as ventas_usd,
+	sum (venta_usd)+ sum(descuentos_usd) as ventas_netas_usd,
+	sum (impuestos_usd) as impuestos_usd,
+	sum (creditos_usd) as creditos_usd,
+	sum (descuentos_usd) as descuentos_usd,
+	sum (costo_usd) as costo_usd,
+	sum (venta_usd) + sum(descuentos_usd) - sum (costo_usd) as gross_margin_usd,
+	(i.inicial+i.final)/2 AS inventario_promedio
+from cte_ventas 
+left join stg.inventory i
+	on i.sku = cte_ventas.producto 
+	and i.tienda = cte_ventas.tienda 
+	and i.fecha = cte_ventas.fecha
+group by 
+	cte_ventas.fecha,
+	cte_ventas.producto,
+	cte_ventas.cantidad,
+	cte_ventas.nombre,
+	cte_ventas.tienda,
+	cte_ventas.pais,
+	cte_ventas.nombre,
+	cte_ventas.categoria,
+	cte_ventas.subcategoria,
+	cte_ventas.subsubcategoria,
+	extract (day from cte_ventas.fecha),
+	cte_ventas.fecha,
+	cte_ventas.mes,
+	cte_ventas.year, 
+	cte_ventas.fiscal_year,
+	cte_ventas.fiscal_quarter,
+	cte_ventas.adjustment,
+	cte_ventas.qty_returned,
+	inventario_promedio
 ;
-select * from stg.source_truth2
+select * from stg.tpintegrador2
 
 -- Cálculo de métricas
 
 -- Ventas brutas, descuentos, impuestos, creditos
 select 
 	sum(ventas_usd) as ventas, 
-	sum(descuento_usd) as descuentos, 
+	sum(descuentos_usd) as descuentos, 
 	sum(impuestos_usd) as impuestos, 
 	sum(creditos_usd) as creditos
-from stg.source_truth2
+from stg.tpintegrador2
 
 -- Ventas netas (inluye ventas y descuentos)
 select
-	sum (ventas_usd) + sum(descuento_usd) as ventas_netas
-from stg.source_truth2
+	sum (ventas_usd) + sum(descuentos_usd) as ventas_netas
+from stg.tpintegrador2
 
 -- Valor final pagado (incluye ventas, descuentos, impuestos y creditos)
 select
-	sum (ventas_usd) - sum(descuento_usd) + sum(impuestos_usd) - sum(creditos_usd) as valor_final
-from stg.source_truth2
+	sum (ventas_usd) - sum(descuentos_usd) + sum(impuestos_usd) - sum(creditos_usd) as valor_final
+from stg.tpintegrador2
 
 -- ROI
 select 
-	sum(ventas_usd)/avg(inventario_promedio*costo_promedio) *100 as ROI
-from stg.source_truth2
-
--- Days on hand
-select
-	producto,
-	inventario_promedio / ventas_promedio_7dias as DOH
-from stg.source_truth2
+	sum(ventas_usd)/avg(inventario_promedio*costo_usd) *100 as ROI
+from stg.tpintegrador2
 
 -- Costos
 select
 	sum(costo_promedio)
-from stg.source_truth2
+from stg.tpintegrador2
 	
-
 -- Margen bruto (gross margin)
 select 
-	sum(ventas_usd) + sum(descuento_usd) + sum(creditos_usd) - sum(costo_promedio*cantidad)
+	sum(ventas_usd) + sum(descuentos_usd) + sum(creditos_usd) - sum(costo_usd*cantidad)
 		as margen_bruto
-from stg.source_truth2
+from stg.tpintegrador2
 
 -- AGM (adjusted gross margin)
 select 
-	sum(ventas_usd) + sum(descuento_usd) + sum(creditos_usd) - sum(costo_promedio*cantidad)+ sum (ingreso_extra)
+	sum(ventas_usd) + sum(descuentos_usd) + sum(creditos_usd) - sum(costo_usd*cantidad)+ sum (adjustment)
 		as AGM
-from stg.source_truth2
+from stg.tpintegrador2
 where nombre like '%PHILIPS%'
 
 -- AOV
 select
-	sum(ventas_usd)/count(orden) as AOV
-from stg.source_truth2
+	sum(ventas_usd)/count(countd_orden) as AOV
+from stg.tpintegrador2
 
 -- Numero de devoluciones
 select
-	count (devoluciones) as cant_devoluciones
-from stg.source_truth2
+	count (qty_returned) as cant_devoluciones
+from stg.tpintegrador2
 
--- Ratio de conversion
-select
-	round(count (orden)*100.0/sum(conteo),2) as ratio_conversion
-from stg.source_truth2
